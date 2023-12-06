86df998046b847687ea9e83ca4679d6b6efe62d7
Dmitry <ivashe4ki@yandex.ru>
2023-12-06 15:41:47 +0300

    3
    3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/navbar/Navbar.jsx'
+import { Popular } from './Components/popular/Popular.jsx';
 
 function App() {
   return (
     <div className="App">
       <header>
         <Navbar></Navbar>
+        <Popular></Popular>
       </header>
     </div>
   );
--- my-app/src/components/popular/Popular.jsx
+++ my-app/src/components/popular/Popular.jsx
@@ -2,10 +2,10 @@ export const Popular = () => {
     return (
         <div className={'px-[4.25rem] flex flex-col bg-white'}>
             <h3 className={'pb-[2.81rem] text-[2.8125rem] font-medium'}>Популярное</h3>
-            <div className={' flex flex-row flex-wrap gap-[1rem]'}>
-                {[1, 2, 3, 4, 5, 6].map((it, idx) =>
+            <div className={'grid grid-cols-4 gap-[1rem]'}>
+                {[1, 2, 3, 4, 5, 6, 7, 8].map((it, idx) =>
                     <div className={'flex flex-col'}>
-                        <img src={'rect1.svg'} className={'w-[350px] rounded-[2.5rem] h-[450px]'}/>
+                        <img src={'rect1.svg'} className={'w-[250px] rounded-[2.5rem] h-[350px]'}/>
                         <div className={'flex flex-col items-start'}>
                             <h3 className={'text-[1.875rem]'}>Кроссовки</h3>
                             <p className={'text-[1.5625rem] text-[#828282]'}>Nike</p>
